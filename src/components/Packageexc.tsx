9a259ed29683218f1828e9beb000d8d2fc330999
mustafa46128 <mustafa128khan@gmail.com>
2024-05-25 18:54:10 +0500

    fix navbar issue

---
+++ src/components/Packageexc.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Packageexc() {
+  return (
+    <div className="px-4">
+      <div className="border border-[#5d5d5d]  mt-4">
+        <div className="px-5 py-4">
+          <h1 className="text-[21px] leading-[28px] font-medium font-Gotham">
+            Package Exclusion:
+          </h1>
+          <ul className="px-3">
+            <li className="text-[13px] leading-[28px] mt-3 font-Gotham">
+              - Airfare to in the nearest airport
+            </li>
+            <li className="text-[13px] leading-[28px] font-Gotham">
+              - Travel insurance
+            </li>
+            <li className="text-[13px] leading-[28px] font-Gotham">
+              - Optional tours and activities not mention in the itinerary
+            </li>
+            <li className="text-[13px] leading-[28px] font-Gotham">
+              - Any pearsonal expenses
+            </li>
+            <li className="text-[15px] font-medium leading-[28px] mt-3 font-Gotham">
+              Price is- 1200
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Packageexc;
