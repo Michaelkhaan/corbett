05e8d955cda83c60e01602ed905a503b643b9f1c
mustafa46128 <mustafa128khan@gmail.com>
2024-06-04 03:28:56 +0500

    data

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function Data() {
   return (
-    <div className='m-4'>
-        <h1 className='text-[#000000] font-extrabold font-Gotham text-[16px] leading-[59px] px-6'>Where do you want to go?</h1>
-    <div>
-    <div className='border border-[#5d5d5d] opacity-#0.000 w-[317px] h-[35px] flex items-center px-6'><p>Travel Date</p></div> 
-    <div className='border border-[#5d5d5d] opacity-#0.000 w-[317px] h-[35px] flex items-center px-6'><p>Number</p></div> 
-    <div className='border border-[#5d5d5d] opacity-#0.000 w-[317px] h-[35px] flex items-center px-6'><p>City</p></div> 
-    </div>
-    </div>
-  )
-}
+    <div className="w-full border border-black px-4">
+      <h1 className="mb-5 text-lg ">Where do you want to go?</h1>
 
+      <input
+        type="text"
+        placeholder="Travel Date"
+        className="px-12 w-full focus:outline-none border border-black py-1 mb-4"
+      />
+
+      <input
+        type="text"
+        placeholder="Number"
+        className=" px-12 w-full focus:outline-none border border-black py-1 mb-4"
+      />
+
+      <input
+        type="text"
+        placeholder="City"
+        className="px-12 w-full focus:outline-none border border-black py-1 mb-4"
+      />
+      <button className=" rounded-full w-full bg-yellow-400 text-start px-12 py-1">
+        Submit
+      </button>
+    </div>
+  );
+}
