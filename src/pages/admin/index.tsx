9438710cdb4da359a882ca3053bf12932155ad4e
ghayas9 <ghayasudin999@gmail.com>
2024-07-14 09:42:31 +0500

    fix

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import Categories from "@/components/admin/Categories";
+import Layout from "@/components/Layout";
+
+function Index() {
+  return (
+    <Layout>
+      <Categories />
+    </Layout>
+  );
+}
+
+export default Index;
