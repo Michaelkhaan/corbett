62466e01441f8ef082160b39f0890cedd3cefa4c
ikramandish777 <ikramandish777@gmail.com>
2024-08-22 03:02:05 +0500

    fixes

--- src/components/OurGuestLoveUs.tsx
+++ src/components/OurGuestLoveUs.tsx
@@ -2,25 +2,27 @@ import Image from "next/image";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-interface Props{
-  className?:string
+interface Props {
+  className?: string;
 }
 
 const testimonials = [
   "Our trip to Jim Corbett was absolutely seamless, thanks to the exceptional services provided! Every detail, from the safari bookings to the cozy accommodations, was meticulously arranged. The team’s local expertise made our experience truly unforgettable. We can't wait to book with them again!",
-  
+
   "We had the most amazing wildlife adventure at Jim Corbett, all thanks to the top-notch service. The personalised attention and well-planned itinerary allowed us to fully immerse ourselves in the beauty of the park. A must-book for anyone seeking a hassle-free and memorable experience!",
 
   "From the moment we arrived, everything was perfectly organised. The knowledgeable guides, comfortable stay, and thrilling safaris made our Jim Corbett trip one of the best we’ve ever had. Highly recommended for anyone looking to explore this stunning destination!",
 
   "Our Jim Corbett experience was incredible, thanks to the flawless arrangements. The team ensured that every aspect of our trip, including transport, safari, and lodging, was taken care of with precision. Their attention to detail made our vacation truly enjoyable and stress-free!",
 
-  "A huge thank you to the team for organising such a wonderful Jim Corbett trip for us. The safari experience was exhilarating, and the hospitality was beyond our expectations. We felt completely taken care of throughout our stay. We highly recommend their services!"
+  "A huge thank you to the team for organising such a wonderful Jim Corbett trip for us. The safari experience was exhilarating, and the hospitality was beyond our expectations. We felt completely taken care of throughout our stay. We highly recommend their services!",
 ];
 
-export default function OurGuestLoveUs({className}:Props) {
+export default function OurGuestLoveUs({ className }: Props) {
   return (
-    <div className={`w-full bg-[url('/bglayers.png')] py-16 px-4s ${className}`}>
+    <div
+      className={`w-full bg-[url('/bglayers.png')] py-16 px-4s ${className}`}
+    >
       <h1 className="text-center text-[30.7px] font-Gotham font-medium mb-10">
         OUR GUEST LOVE US
       </h1>
@@ -50,8 +52,7 @@ export default function OurGuestLoveUs({className}:Props) {
       >
         {/* <SwiperSlide className="hidden" /> */}
         {testimonials?.map((item, index) => (
-          <SwiperSlide key={index}
-          >
+          <SwiperSlide key={index}>
             <div className="flex items-center gap-4">
               <Image
                 src="Layer.png"
@@ -61,7 +62,7 @@ export default function OurGuestLoveUs({className}:Props) {
                 className="w-8 aspect-square object-cover rounded-full"
               />
 
-                <p className="text-xs font-Gotham ">{item}</p>
+              <p className="text-xs font-Gotham ">{item}</p>
             </div>
           </SwiperSlide>
         ))}
